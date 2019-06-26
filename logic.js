0869267401ecbc4177b031b709fa4b5b3c0a0de2
Noam katz <nuna@noams-mbp.mynet>
2019-06-26 18:17:40 +0300

    first push

---
+++ logic.js
@@ -0,0 +1,34 @@
+const tweeter = function(){
+    
+    const _posts = [
+        {
+            text: "First post!",
+            id: "p1",
+            comments: [
+                { id: "c1", text: "First comment on first post!" },
+                { id: "c2", text: "Second comment on first post!!" },
+                { id: "c3", text: "Third comment on first post!!!" }
+            ]
+        },
+        {
+            text: "Aw man, I wanted to be first",
+            id: "p2",
+            comments: [
+                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
+                { id: "c5", text: "Yeah, believe in yourself!" },
+                { id: "c6", text: "Haha second place what a joke." }
+            ]
+        }
+        ];
+    const postidCounter = _posts.length
+    const commentIdCounter = function(){
+        for(let index in _posts){
+            let counter = 0 
+            counter += _posts[index].comments.length
+        }
+    }
+
+}
+
+
+
