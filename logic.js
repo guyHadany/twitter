9b08c13941c052aa63dcf206831c0535152ba184
Noam katz <nuna@noams-mbp.mynet>
2019-06-28 09:31:32 +0300

    not working yet

--- logic.js
+++ logic.js
@@ -1,26 +1,7 @@
 const Tweeter = function(){
     
-    const _posts = [
-        {
-            text: "First post!",
-            id: "p1",
-            comments: [
-                { id: "c1", text: "First comment on first post!" },
-                { id: "c2", text: "Second comment on first post!!" },
-                { id: "c3", text: "Third comment on first post!!!" }
-            ]
-        },
-        {
-            text: "Aw man, I wanted to be first",
-            id: "p2",
-            comments: [
-                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
-                { id: "c5", text: "Yeah, believe in yourself!" },
-                { id: "c6", text: "Haha second place what a joke." }
-            ]
-        }
-        ];
-    let postidCounter = 0
+    const _posts = []
+    let postidCounter = 1
     let commentIdCounter = 0
     const getPosts = function(){
         return _posts
@@ -66,6 +47,7 @@ const Tweeter = function(){
     }    
     return {
         getPosts,
+        addPost,
         removePost,
         addComment,
         removeComment
--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-// const tweeter = Tweeter()
-// const renderer = Renderer()
+const post = function(){
+    if($("#input").val()){
+    tweeter.addPost($("#input").val())
+    $("#input").val("")
 
-// renderer.renderPosts(tweeter.getPosts())
+    renderer.renderPosts(tweeter.getPosts())
+    }
+}
 
 
-$()
+
+const deletePost = function(){
+// console.log("hello")
+// console.log($(this).closest(".post").data().id)
+    tweeter.removePost($(this).closest(".post").data().id)
+    renderer.renderPosts(tweeter.getPosts())
+}
+$(".post").on("click", ".delete", deletePost)
+
+
+const tweeter = Tweeter()
+const renderer = Renderer()
+
+renderer.renderPosts(tweeter.getPosts())
+
+
